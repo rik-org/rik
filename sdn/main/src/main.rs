3e667f81d9302a5cad160f2d929ed683d12c0e24
theo.peltier <theo.peltier@etu.umontpellier.fr>
2021-07-05 17:14:14 +0200

    feat: begin clap

--- sdn/main/src/main.rs
+++ sdn/main/src/main.rs
@@ -1,51 +1,62 @@
-use futures::stream::TryStreamExt;
-use ipnetwork::IpNetwork;
-use rtnetlink::{new_connection, Error};
+use clap::{AppSettings, Clap};
+use rtnetlink::new_connection;
 
 #[tokio::main]
-async fn create_veth(link_name1: &str, link_name2: &str) -> Result<(), String> {
+async fn create_veth() -> Result<(), String> {
     let (connection, handle, _) = new_connection().unwrap();
     tokio::spawn(connection);
     handle
         .link()
         .add()
-        .veth(link_name1.into(), link_name2.into())
+        .veth("veth-rs-1".into(), "veth-rs-2".into())
         .execute()
         .await
         .map_err(|e| format!("{}", e))
 }
 
-#[tokio::main]
-async fn add_address(link_name: &str, ip: IpNetwork) -> Result<(), Error> {
-    let (connection, handle, _) = new_connection().unwrap();
-    tokio::spawn(connection);
+#[derive(Clap)]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Opts {
+    /// Sets a custom config file. Could have been an Option<T> with no default too
+    #[clap(short, long, default_value = "default.conf")]
+    namespace_path: String,
 
-    let mut links = handle
-        .link()
-        .get()
-        .set_name_filter(link_name.to_string())
-        .execute();
-    if let Some(link) = links.try_next().await? {
-        handle
-            .address()
-            .add(link.header.index, ip.ip(), ip.prefix())
-            .execute()
-            .await?
-    }
-    Ok(())
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+
+}
+
+#[derive(Clap)]
+enum SubCommand {
+    #[clap(version = "1.3", author = "Someone E. <someone_else@other.com>")]
+    Create(create),
+}
+
+/// A subcommand for controlling testing
+#[derive(Clap)]
+struct create {
+    /// Print debug info
+    #[clap(short)]
+    debug: bool,
+
 }
 
 fn main() -> Result<(), String> {
-    //mock variables
-    let link_name1 = "veth-rs-1";
-    let link_name2 = "veth-rs-2";
-    let ip: IpNetwork = "1.1.1.1".parse().unwrap_or_else(|_| {
-        eprintln!("invalid address");
-        std::process::exit(1);
-    });
+    let opts: Opts = Opts::parse();
 
-    let _ = create_veth(link_name1, link_name2);
-    let _ = add_address(link_name1, ip);
 
-    Ok(())
+    // You can handle information about subcommands by requesting their matches by name
+    // (as below), requesting just the name used, or both at the same time
+    match opts.subcmd {
+        SubCommand::Create(t) => {
+            if t.debug {
+                println!("Printing debug info...");
+            } else {
+                println!("Printing normally...");
+            }
+        }
+    }
+
+
+    create_veth()
 }
