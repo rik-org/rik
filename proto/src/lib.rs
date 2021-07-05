b0a2787dbc1cd5776720cfa970499bd1b40709dc
Protocole <burgoni.alexandre@protonmail.com>
2021-07-05 19:46:29 +0200

    feat(protos): release 0.1.2

--- proto/src/lib.rs
+++ proto/src/lib.rs
@@ -1,3 +1,6 @@
+use std::fmt;
+use common::{WorkerMetric, InstanceMetric};
+
 pub mod common {
     tonic::include_proto!("common");
 }
@@ -8,6 +11,18 @@ pub mod worker {
 
 pub mod controller {
     tonic::include_proto!("controller");
+}
+
+impl fmt::Display for WorkerMetric {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {})", self.status, self.metrics)
+    }
+}
+
+impl fmt::Display for InstanceMetric {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {})", self.status, self.metrics)
+    }
 }
 
 pub extern crate protobuf;
