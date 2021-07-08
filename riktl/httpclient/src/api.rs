2c67a3cb84105f1b4617e4037a70b2ac6b72a03e
Julian Labatut <64641535+jlabatut@users.noreply.github.com>
2021-07-08 17:53:31 +0200

    feat: add riktl v1.0 (#8)
    * initial commit riktl project
    
    * Update Cargo.toml
    
    * feat: httpclient module
    
    create module for communicate with the controller
    
    no breaking change
    
    * feat: add config file
    
    add config file with config module to read controller uri from yaml file.
    
    Breaking change: riktl can't send http request without rik.config.yml at riktl project root
    
    * fix: fix get request
    
    get request return response
    
    no breaking change
    
    * feat: workloadService
    
    add workload service to create get and delete workload
    
    No breaking change
    
    * feat(cli): add arguments
    
    * docs(cli): update README.md
    
    * feat(cli): add create/delete/get workload commands
    
    * feat(cli): add create/delete/get instance commands
    
    * feat: configfile env var
    
    take into account the RIKCONFIG environment variable.
    
    No breaking change
    
    * feat: send replicas to controller
    
    add replicas number in body of instance post request
    
    No breaking change
    
    * feat(riktl): add replicas option (#6)
    
    * docs(riktl): update README
    
    * feat(riktl): add replicas option
    
    Co-authored-by: Julian Labatut <julian.labatut@etu.umontpellier.fr>
    
    * Update README.md
    
    * add readme
    
    * add readme
    
    * feat(riktl): add replicas option, add instance status, print error messages (#7)
    
    * docs(riktl): update README
    
    * feat(riktl): add replicas option
    
    * fix(riktl): add instance status, print error messages
    
    * fix(riktl): fix typo status table
    
    Co-authored-by: Julian Labatut <julian.labatut@etu.umontpellier.fr>
    
    Co-authored-by: mfl ops <mfldev@polddddee.fr>
    Co-authored-by: Mathias Flagey <61873164+NelopsisCode@users.noreply.github.com>
    Co-authored-by: NelopsisCode <mathiasflagey1201@gmail.com>
    Co-authored-by: Julian Labatut <julian.labatut@etu.umontpellier.fr>

---
+++ riktl/httpclient/src/api.rs
@@ -0,0 +1,114 @@
+extern crate reqwest;
+use crate::config::Config;
+use crate::ApiError;
+use serde_json::Value;
+
+#[derive(Debug)]
+pub struct ApiRequest {
+    uri: String,
+    endpoint: String,
+    header: Option<String>,
+    body: Option<String>,
+}
+
+impl ApiRequest {
+    pub fn new(
+        endpoint: String,
+        body: Option<String>,
+        header: Option<String>,
+    ) -> Result<Self, ApiError> {
+        let uri = Config::get_uri()?;
+        let api_request = ApiRequest {
+            endpoint,
+            header,
+            body,
+            uri,
+        };
+
+        Ok(api_request)
+    }
+
+    pub fn get(self) -> Result<Vec<Value>, ApiError> {
+        //Get request
+        match reqwest::blocking::get(format!("{}{}", self.uri, self.endpoint)) {
+            Ok(response) => {
+                if response.status() == reqwest::StatusCode::OK {
+                    match response.text() {
+                        Ok(text) => {
+                            let body_value: Vec<Value> = serde_json::from_str(&text).unwrap();
+                            Ok(body_value)
+                        }
+                        Err(_) => Err(ApiError::CantReadResponse),
+                    }
+                } else {
+                    Err(ApiError::BadStatus(response.status()))
+                }
+            }
+            Err(_) => Err(ApiError::BadURI(self.uri)),
+        }
+    }
+
+    pub fn post(self) -> Result<Value, ApiError> {
+        if self.body == None {
+            return Err(ApiError::EmptyBody);
+        }
+        //Post Request
+        let client = reqwest::blocking::Client::new();
+        if let Some(body) = self.body {
+            let body_value: Value = serde_json::from_str(&body).unwrap();
+            match client
+                .post(format!("{}{}", self.uri, self.endpoint))
+                .json(&body_value)
+                .header("Content-Type", "application/json")
+                .send()
+            {
+                Ok(response) => {
+                    if response.status() == reqwest::StatusCode::CREATED
+                        || response.status() == reqwest::StatusCode::NO_CONTENT
+                        || response.status() == reqwest::StatusCode::OK
+                    {
+                        match response.text() {
+                            Ok(text) => {
+                                if text == "" {
+                                    return Ok(Value::Null);
+                                }
+                                let body_value: Value = serde_json::from_str(&text).unwrap();
+                                Ok(body_value)
+                            }
+                            Err(_) => Err(ApiError::CantReadResponse),
+                        }
+                    } else {
+                        let error = response.status().clone();
+                        println!("{}", &response.text().unwrap());
+                        Err(ApiError::BadStatus(error))
+                    }
+                }
+                Err(_) => return Err(ApiError::BadURI(self.uri)),
+            }
+        } else {
+            return Err(ApiError::EmptyBody);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::api::ApiRequest;
+    use crate::ApiError;
+
+    #[test]
+    fn post_empty_body_return_error() -> Result<(), ApiError> {
+        let api_request = ApiRequest::new("/NelopsisCode".to_string(), None, None)?;
+        assert!(api_request.post().is_err());
+
+        Ok(())
+    }
+    #[test]
+    fn invalid_endpoint_return_error() -> Result<(), ApiError> {
+        let api_request =
+            ApiRequest::new("/fez5f4e6157ae6f4faf7ef5aze4f3fa56".to_string(), None, None)?;
+        assert!(api_request.get().is_err());
+
+        Ok(())
+    }
+}
---
+++ riktl/src/cli.rs
@@ -0,0 +1,155 @@
+extern crate clap;
+
+use clap::{App, Arg};
+
+#[derive(Debug)]
+pub struct Cli {
+    pub action: Action,
+    pub entity: Entity,
+    pub workload_id: String,
+    pub instance_id: String,
+    pub file: String,
+    pub replicas: String,
+}
+
+#[derive(Debug)]
+pub enum CliError {
+    MissingArg(String),
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Action {
+    CREATE,
+    DELETE,
+    GET,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Entity {
+    INSTANCE,
+    WORKLOAD,
+}
+
+const CMD_CREATE: &'static str = "create";
+const CMD_DELETE: &'static str = "delete";
+const CMD_GET: &'static str = "get";
+const CMD_INSTANCE: &'static str = "instance";
+const CMD_WORKLOAD: &'static str = "workload";
+
+impl Cli {
+    pub fn new() -> Result<Self, CliError> {
+        let args = App::new("riktl")
+            .author("Polytech Montpellier - DO 2023")
+            .version(&*format!("{}{}", "v", clap::crate_version!()))
+            .about("RIK Command Line Interface - A rustlang based cloud orchestrator")
+            .arg(
+                Arg::new("action")
+                    .required(true)
+                    .about("The action to perform")
+                    .possible_values(&[CMD_CREATE, CMD_DELETE, CMD_GET])
+                    .index(1),
+            )
+            .arg(
+                Arg::new("entity")
+                    .required(true)
+                    .about("The entity to handle")
+                    .possible_values(&[CMD_INSTANCE, CMD_WORKLOAD])
+                    .index(2),
+            )
+            .arg(
+                Arg::new("file")
+                    .short('f')
+                    .long("file")
+                    .about("The YAML file containing the workload description")
+                    .takes_value(true)
+                    .required_if_eq_all(&[("action", CMD_CREATE), ("entity", CMD_WORKLOAD)]),
+            )
+            .arg(
+                Arg::new("workload_id")
+                    .short('w')
+                    .long("workload")
+                    .about("The target workload id")
+                    .takes_value(true)
+                    .required_if_eq_all(&[("action", CMD_CREATE), ("entity", CMD_INSTANCE)]),
+            )
+            .arg(
+                Arg::new("instance_id")
+                    .short('i')
+                    .long("instance")
+                    .about("The target instance id")
+                    .takes_value(true)
+                    .required_if_eq_all(&[("action", CMD_DELETE), ("entity", CMD_INSTANCE)]),
+            )
+            .arg(
+                Arg::new("replicas")
+                    .short('n')
+                    .long("replicas")
+                    .about("The number of replicas")
+                    .takes_value(true),
+            )
+            .get_matches();
+
+        let action = match args
+            .value_of("action")
+            .expect("Missing action argument (create | delete | get)")
+        {
+            CMD_CREATE => Action::CREATE,
+            CMD_DELETE => Action::DELETE,
+            CMD_GET => Action::GET,
+            _ => panic!(),
+        };
+
+        let entity = match args
+            .value_of("entity")
+            .expect("Missing entity argument (workload | instance")
+        {
+            CMD_WORKLOAD => Entity::WORKLOAD,
+            CMD_INSTANCE => Entity::INSTANCE,
+            _ => panic!(),
+        };
+
+        let file: &str;
+        match args.value_of("file") {
+            Some(f) => file = f,
+            None => file = "",
+        }
+
+        let replicas: &str;
+        match args.value_of("replicas") {
+            Some(r) => replicas = r,
+            None => replicas = "",
+        }
+
+        let workload_id: &str;
+        match args.value_of("workload_id") {
+            Some(i) => workload_id = i,
+            None => {
+                if action == Action::DELETE && entity == Entity::WORKLOAD {
+                    return Err(CliError::MissingArg(String::from("workload")));
+                } else {
+                    workload_id = ""
+                }
+            }
+        }
+
+        let instance_id: &str;
+        match args.value_of("instance_id") {
+            Some(i) => instance_id = i,
+            None => {
+                if action == Action::DELETE && entity == Entity::INSTANCE {
+                    return Err(CliError::MissingArg(String::from("instance")));
+                } else {
+                    instance_id = ""
+                }
+            }
+        }
+        Ok(Cli {
+            action,
+            entity,
+            file: String::from(file),
+            workload_id: String::from(workload_id),
+            instance_id: String::from(instance_id),
+            replicas: String::from(replicas),
+        })
+    }
+}
