1f8e7512d3cec30d9c02b5bf9210d105bf3b86d2
Thomas Gouveia <47056759+thomasgouveia@users.noreply.github.com>
2021-07-08 15:57:13 +0200

    feat: riklet component  v1 (#2)
    * chore: setup folders
    
    * build(#7): start implementing image pulling process
    
    * feat: run container with runc
    
    * chore: node-metrics init
    
    * refactor: rework skopeo & umoci
    
    * fix: uncomment container creation in main.rs
    
    * feat(daemon): implemented worker registration and wip send status updates
    
    * tests: Add unit test to container runtime
    
    * feat(daemon): threaded registration and working status updates
    
    * tests: add more unit tests to Runc implementation & add delete & kill containers feature
    
    * example: print what it is needed
    
    * wip
    
    * feat: to_json()
    
    * chore: todos added
    
    * feat: Add TOML configuration file for Riklet
    
    * fix: lock configuration file location
    
    * tests: add unit tests for configuration
    
    * fix(daemon): separation between workloads and status sending
    
    * feat: integration of node-metrics
    
    * feat: integration of metrics with riklet struct
    
    * feat: complete integration
    
    * fix: remove comments in main.rs
    
    * feat: Add clap for CLI arguments & improve code
    
    * docs: add docstring
    
    * feat: add hostname for node registration
    
    * feat: add real metrics for node status
    
    * feat: add identifier in metrics
    
    * fix: bootstrap config on load
    
    * refactor: move node updater into a method
    
    * fix: umoci error causes failed container
    
    * feat: implement image pull policy
    
    * feat(riklet): added riklet service with debian package
    
    * feat: improve pulling policy for images
    
    * fix: added restart sec in service
    
    * feat: workload unschedule and metrics update
    
    * feat(riklet): release 1.0.0
    
    Co-authored-by: Thomas Gouveia <thomas.gouveia@ioterop.com>
    Co-authored-by: sylvain.reynaud <sylvain.reynaud@etu.umontpellier.fr>
    Co-authored-by: Hugo Amalric <hugoamalric@gmail.com>

---
+++ riklet/src/crates/oci/src/image.rs
@@ -0,0 +1,78 @@
+use std::hash::Hash;
+use shared::utils::generate_hash;
+use std::path::{PathBuf};
+
+#[derive(Debug, Hash)]
+pub enum ImagePullPolicy {
+    IfNotPresent,
+    Always
+}
+
+#[derive(Debug, Hash)]
+pub struct Image {
+    pub oci: String,
+    pub name: String,
+    pub tag: String,
+    pub bundle: Option<PathBuf>,
+    pub pull_policy: ImagePullPolicy
+}
+
+impl Image {
+    /// Create a new image
+    pub fn from(img: &str) -> Self {
+
+        let splitted_image: Vec<&str> = img.split(':').collect();
+        let image_name = *splitted_image.get(0).unwrap();
+        let image_tag = *splitted_image.get(1).unwrap();
+
+        Image {
+            oci: String::from(img),
+            name: String::from(image_name),
+            tag: String::from(image_tag),
+            bundle: None,
+            pull_policy: ImagePullPolicy::IfNotPresent
+        }
+    }
+
+    pub fn should_be_pulled(&self, directory: &PathBuf) -> bool {
+        match &self.pull_policy {
+            ImagePullPolicy::IfNotPresent => !directory.join(&self.get_uuid()).exists(),
+            ImagePullPolicy::Always => true
+        }
+    }
+    
+    pub fn get_uuid(&self) -> String {
+        format!("{}-{}", self.name, self.get_hash())
+    }
+
+    pub fn set_bundle(&mut self, bundle: &str) {
+        self.bundle = Some(PathBuf::from(bundle));
+    }
+
+    pub fn get_hash(&self) -> u64 {
+        generate_hash(self)
+    }
+
+    pub fn get_hashed_oci(&self) -> String {
+        format!("{}-{}:{}", self.name, self.get_hash(), self.tag)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::image::Image;
+
+    #[test]
+    fn test_it_parse_a_image_string() {
+        let image_str = "alpine:latest";
+
+        let image = Image::from(image_str);
+
+        assert_eq!(image.name, "alpine");
+        assert_eq!(image.tag, "latest");
+    }
+}
+
+
+
+
---
+++ riklet/src/crates/oci/src/skopeo.rs
@@ -0,0 +1,200 @@
+use std::time::Duration;
+use std::path::PathBuf;
+use shared::utils::find_binary;
+use snafu::{OptionExt, ResultExt};
+use crate::*;
+use std::process::Stdio;
+use tokio::process::Command;
+use log::debug;
+use snafu::ensure;
+use serde::{Serialize, Deserialize};
+
+#[derive(Default, Debug, Clone, Serialize, Deserialize, Eq, PartialEq)]
+pub struct SkopeoConfiguration {
+    pub debug: bool,
+    pub insecure_policy: bool,
+    pub command: Option<PathBuf>,
+    pub images_directory: Option<PathBuf>,
+    pub override_arch: Option<String>,
+    pub override_os: Option<String>,
+    pub override_variant: Option<String>,
+    pub policy: Option<String>,
+    pub registries: Option<PathBuf>,
+    pub tmp_dir: Option<PathBuf>,
+    pub timeout: Option<Duration>,
+}
+
+#[derive(Debug)]
+pub struct Skopeo {
+    command: PathBuf,
+    timeout: Duration,
+    images_directory: PathBuf,
+    debug: bool,
+    insecure_policy: bool,
+    override_arch: Option<String>,
+    override_os: Option<String>,
+    override_variant: Option<String>,
+    policy: Option<String>,
+    registries: Option<PathBuf>,
+    tmp_dir: Option<PathBuf>,
+}
+
+impl Skopeo {
+
+    pub fn new(config: SkopeoConfiguration) -> Result<Self> {
+        let command = config
+            .command
+            .or_else(|| find_binary("skopeo"))
+            .context(SkopeoNotFoundError {})?;
+
+        let timeout = config.timeout.or(Some(Duration::from_millis(5000))).unwrap();
+
+        let images_directory = config
+            .images_directory
+            .unwrap()
+            .canonicalize()
+            .context(InvalidPathError {})?;
+
+        debug!("Skopeo initialized.");
+
+        Ok(Self {
+            command,
+            timeout,
+            images_directory,
+            debug: config.debug,
+            insecure_policy: config.insecure_policy,
+            override_arch: config.override_arch,
+            override_os: config.override_os,
+            override_variant: config.override_variant,
+            policy: config.policy,
+            registries: config.registries,
+            tmp_dir: config.tmp_dir
+        })
+    }
+
+    fn get_pull_path(&self, directory: &String) -> String {
+        format!(
+            "oci:{}/{}",
+            self.images_directory.to_str().unwrap(),
+            directory
+        )
+    }
+
+    pub async fn copy(&self, src: &String, uuid: &String, opts: Option<&CopyArgs>) -> Result<String> {
+        let mut args = vec![String::from("copy"), src.clone()];
+        Self::append_opts(&mut args, opts.map(|opts| opts as &dyn Args))?;
+
+        let image_pull_path = self.get_pull_path(uuid);
+
+        args.push(image_pull_path.clone());
+
+        self.exec(&args).await?;
+
+        let splitted = image_pull_path.split(":").collect::<Vec<&str>>();
+        Ok(String::from(*splitted.get(1).unwrap()))
+    }
+}
+
+impl Args for Skopeo {
+    fn args(&self) -> Result<Vec<String>> {
+        let mut args: Vec<String> = Vec::new();
+
+        if self.debug {
+            args.push(String::from("--debug"));
+        }
+
+        if self.insecure_policy {
+            args.push(String::from("--insecure-policy"));
+        }
+
+        if let Some(override_arch) = self.override_arch.clone() {
+            args.push(String::from("--override-arch"));
+            args.push(override_arch)
+        }
+
+        if let Some(override_os) = self.override_os.clone() {
+            args.push(String::from("--override-os"));
+            args.push(override_os);
+        }
+
+        if let Some(override_variant) = self.override_variant.clone() {
+            args.push(String::from("--override-variant"));
+            args.push(override_variant);
+        }
+
+        if let Some(policy) = self.policy.clone() {
+            args.push(String::from("--policy"));
+            args.push(policy);
+        }
+
+        if let Some(registries) = self.registries.clone() {
+            let registries = registries.canonicalize().context(InvalidPathError {})?;
+            args.push(String::from("--registries.d"));
+            args.push(String::from(registries.to_str().unwrap()));
+        }
+
+        if let Some(tmp_dir) = self.tmp_dir.clone() {
+            let tmp_dir = tmp_dir.canonicalize().context(InvalidPathError {})?;
+            args.push(String::from("--tmpdir"));
+            args.push(String::from(tmp_dir.to_str().unwrap()));
+        }
+
+        Ok(args)
+    }
+}
+
+#[async_trait]
+impl Executable for Skopeo {
+    async fn exec(&self, args: &[String]) -> Result<String> {
+        let args = self.concat_args(args)?;
+        let process = Command::new(&self.command)
+            .args(&args.clone())
+            .stdin(Stdio::null())
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn()
+            .context(ProcessSpawnError {})?;
+
+        debug!("{} {}", self.command.to_str().unwrap(), &args.clone().join(" "));
+
+        let result = tokio::time::timeout(self.timeout, process.wait_with_output())
+            .await
+            .context(SkopeoCommandTimeoutError {})?
+            .context(SkopeoCommandError {})?;
+
+        let stdout = String::from_utf8(result.stdout.clone()).unwrap();
+        let stderr = String::from_utf8(result.stderr.clone()).unwrap();
+
+        if stderr != "" {
+            error!("Skopeo error : {}", stderr);
+        }
+
+        ensure!(
+            result.status.success(),
+            SkopeoCommandFailedError {
+                stdout: stdout,
+                stderr: stderr
+            }
+        );
+
+        Ok(stdout)
+    }
+}
+
+pub struct CopyArgs {
+    pub auth_file: Option<PathBuf>
+}
+
+impl Args for CopyArgs {
+    fn args(&self) -> Result<Vec<String>> {
+        let mut args: Vec<String> = Vec::new();
+
+        if let Some(auth_file) = self.auth_file.clone() {
+            let auth_file = auth_file.canonicalize().context(InvalidPathError {})?;
+            args.push(String::from("--authfile"));
+            args.push(String::from(auth_file.to_str().unwrap()))
+        }
+
+        Ok(args)
+    }
+}
