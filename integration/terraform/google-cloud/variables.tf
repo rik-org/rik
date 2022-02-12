d14f7cbcb370331732201ceb2a05d99d3ff3a94f
thomasgouveia <programmationthomas@gmail.com>
2022-02-12 14:16:01 +0100

    integration: add GCP terraform

--- integration/terraform/google-cloud/variables.tf
+++ integration/terraform/google-cloud/variables.tf
@@ -13,3 +13,9 @@ variable "workers_count" {
   description = "The number of workers to provision."
   default     = 1
 }
+
+variable "cluster_name" {
+  type        = string
+  description = "The name of the cluster."
+  default     = "rik"
+}
