e6245f42f5b487415e5654ed3b0c7b1d216f200a
leofvo <leofvo@proton.me>
2023-02-20 09:48:02 +0100

    test: worker repository

--- controller/src/core/worker_repository.rs
+++ controller/src/core/worker_repository.rs
@@ -54,3 +54,63 @@ impl WorkerRepository for WorkerRepositoryImpl {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::tests::fixtures::db_connection;
+    use rstest::rstest;
+
+    #[rstest]
+    fn test_fetch_worker_address_ok(db_connection: std::sync::Arc<RikDataBase>) {
+        let connection = db_connection.open().unwrap();
+        connection.execute("DELETE FROM cluster", []).unwrap();
+        let worker_id = "test-worker";
+        let address = "http://localhost:8080";
+        let worker_repository = WorkerRepositoryImpl::new(db_connection);
+        worker_repository
+            .register_worker(worker_id.to_string(), address.to_string())
+            .unwrap();
+
+        let fetched_address = worker_repository
+            .fetch_worker_address(worker_id.to_string())
+            .unwrap();
+        assert_eq!(fetched_address, address);
+    }
+
+    #[rstest]
+    fn test_fetch_worker_address_not_found(db_connection: std::sync::Arc<RikDataBase>) {
+        let worker_repository = WorkerRepositoryImpl::new(db_connection);
+        let result = worker_repository.fetch_worker_address("test-worker".to_string());
+        assert!(result.is_err());
+    }
+
+    #[rstest]
+    fn test_register_worker_ok(db_connection: std::sync::Arc<RikDataBase>) {
+        let worker_repository = WorkerRepositoryImpl::new(db_connection);
+        let worker_id = "test-worker";
+        let address = "http://localhost:8080";
+        let result = worker_repository.register_worker(worker_id.to_string(), address.to_string());
+        assert!(result.is_ok());
+    }
+
+    #[rstest]
+    fn test_update_worker_addr(db_connection: std::sync::Arc<RikDataBase>) {
+        let worker_repository = WorkerRepositoryImpl::new(db_connection);
+        let worker_id = "test-worker";
+        let address = "http://localhost:8080";
+        worker_repository
+            .register_worker(worker_id.to_string(), address.to_string())
+            .unwrap();
+
+        let new_address = "http://localhost:8081";
+        worker_repository
+            .register_worker(worker_id.to_string(), new_address.to_string())
+            .unwrap();
+
+        let fetched_address = worker_repository
+            .fetch_worker_address(worker_id.to_string())
+            .unwrap();
+        assert_eq!(fetched_address, new_address);
+    }
+}
